e74b7cea1c5179a95110bd95c9e2a9ecc55407eb
Alex Lee <alexwlee.web@gmail.com>
2016-02-08 16:43:13 +1100

    Multiple textarea code completed. Testing begins...

--- app/assets/javascripts/levels.js
+++ app/assets/javascripts/levels.js
@@ -1,8 +1,34 @@
 var app = app || {};
 
-app.answer = [
-    { attr: "border-radius", val: "50%;" },
-    { attr: "border-radius", val: "75px;" },
-    { attr: "background-color", val: "rgb(255, 0, 0);" },
-    { attr: "background", val: "rgb(255, 0, 0) none repeat scroll 0% 0% / auto padding-box border-box;" }
-];
+app.levels = {
+    "Basic Shapes": {}
+};
+
+app.levels["Basic Shapes"].level1 = {
+    answers : [
+        [
+            { attr: "border-radius", val: "50%;" },
+            { attr: "border-radius", val: "75px;" },
+            { attr: "background-color", val: "rgb(255, 0, 0);" },
+            { attr: "background", val: "rgb(255, 0, 0) none repeat scroll 0% 0% / auto padding-box border-box;" }
+        ],
+        [
+            { attr: "border-color", val: "rgb(0, 255, 0) none repeat scroll 0% 0% / auto padding-box border-box;" }
+        ]
+    ],
+    question: "",
+    user: '<div class="user-circle" id="user-circle"></div>',
+    css: '<div class="circle"></div>',
+    code: [
+        {
+            before: "#circle {<br/>  height: 150px;<br/>  width: 150px;<br/>  border: 5px solid black;",
+            after: "",
+            rows: 2
+        },
+        {
+            before: "/* More code here */",
+            after: "}",
+            rows: 1
+        }
+    ]
+};
--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,8 +1,11 @@
 var app = app || {};
 
 app.numLines = 2;
+app.$cssElement = $("#css");
+app.$codeElement = $("#cssbox");
+app.$codeBox = $(".code");
 
-app.generateLineNumbers = function(lines) {
+app.generateLineNumbers = function(lines, $element) {
     var html = '<div class="line-numbers">';
 
     for (var i = 1; i <= lines; i++) {
@@ -10,22 +13,22 @@ app.generateLineNumbers = function(lines) {
     }
 
     html += "</div>";
+    $element.css("height", (lines * 26) + "px");
     return html;
 }
 
-app.generateCodebox = function(htmlBefore, htmlAfter, lines, $element ) {
-    var html = app.generateLineNumbers(lines);
-    html += "<pre>" + htmlBefore + "</pre>";
-    html += '<textarea class="code" autofocus></textarea>';
+app.generateCodebox = function(htmlBefore, htmlAfter, rows) {
+    var html = "<pre>" + htmlBefore + "</pre>";
+    html += '<textarea class="code" rows="' + rows + '" autofocus></textarea>';
     html += "<pre>" + htmlAfter + "</pre>";
-    $element.append(html);
+    app$cssElement.append(html);
 }
 
-app.generateCssView = function($element, htmlElement, id) {
+app.generateCssView = function($element, htmlCode, id) {
     var html = '<div class="cssview" id="' + id + '">';
-    html += htmlElement;
+    html += htmlCode;
     html += "</div>";
-    $element.append(html);
+    app$codeElement.append(html);
 }
 
 app.checkline = function(regex, line) {
@@ -45,13 +48,13 @@ app.checkline = function(regex, line) {
 
     Makes a deep comparison between the users answer and the actual answer to check if the user is correct.
 */
-app.checkAnswer = function(userAnswer, answer, $cssElement) {
+app.checkAnswer = function(userAnswer, answer) {
 
     match = _.every(userAnswer, function (user_answer) {
         return _.some(answer, function (_answer) {
 
             if ( user_answer.attr && user_answer.attr === _answer.attr ) {
-                var userAttr = $cssElement.css(user_answer.attr);
+                var userAttr = app$cssElement.css(user_answer.attr);
 
                 var userCss = app.checkline( /[-+]?[0-9]*\.?[0-9]*(.*)/, userAttr );
                 var answerCss = app.checkline( /[-+]?\d*\.?\d*(.*);/, _answer.val );
@@ -96,6 +99,38 @@ app.captureCode = function($element) {
         }
 
     return userAnswer;
+}
+
+app.generateLevel = function(numTextareas, category, level) {
+    app.generateCssView(app.html[level].user, "user-css");
+    app.generateCssView(app.html[level].css, "example-css");
+
+    app.generateLineNumbers(20, $codeElement);
+
+    var code = app.levels[category]["level"+level].code;
+    _.each(code, function(c) {
+        app.generateCodebox(c.before, c.after, c.rows);
+    });
+
+    answers = [];
+
+    $codeBox.each(function() {
+        $(this).on("keydown", function(event) {
+            var linesOfCode = $(this).val().split("\n").length;
+            if (event.which === 13 && linesOfCode === app.numLines) { return false; }
+        });
+
+        $(this).on("keyup", function() {
+            $cssElement.attr("style", $userCode.val() );
+            var userAnswer = app.captureCode($userCode);
+            answers += app.checkAnswer(userAnswer, app.answer, $cssElement);
+        });
+
+        if ( _.every(answers) ) {
+            alert("Yay, you got it correct!");
+        }
+
+    });
 }
 
 
