568ab40ccb04b6b21364ec201d219dbe2055b4c1
Alex Lee <alexwlee.web@gmail.com>
2016-02-11 16:08:30 +1100

    Animations Category added

--- app/assets/javascripts/animations.js
+++ app/assets/javascripts/animations.js
@@ -9,15 +9,22 @@ app.levels["animations"].level1 = {
             { attr: "background", val: "rgb(255, 0, 0) none repeat scroll 0% 0% / auto padding-box border-box;" }
         ]
     ],
-    question: "Make a red circle",
-    user: '<div class="user-circle center-shape" id="user-circle"></div>',
-    css: '<div class="pacman-top"></div><div class="pacman-bottom"></div>',
+    question: "Make pacman eat",
+    user: '<div class="center-shape"><div class="user-pacman-top" id="pacman-top"></div><div class="user-pacman-bottom" id="pacman-bottom"></div></div>',
+    css: '<div class="center-shape"><div class="pacman-top"></div><div class="pacman-bottom"></div></div>',
     code: [
         {
-            cssSelector: "#user-circle",
-            before: "#circle {<br/>  height: 150px;<br/>  width: 150px;<br/>  border: 5px solid black;",
-            after: "}",
-            rows: 2
+            cssSelector: "#pacman-top",
+            before: "#pacman-top {\n  height: 50px;\n  width: 100px;\n  border-radius: 50px 50px 0 0;\n  -webkit-transform: rotate(-35deg);\n  -moz-transform: rotate(-35deg);\n  transform: rotate(-35deg);\n  background-color: yellow;",
+            after: "}\n",
+            rows: 1
+        },
+
+        {
+            cssSelector: "#pacman-bottom",
+            before: "#pacman-bottom {\n  height: 50px;\n  width: 100px;\n  border-radius: 50px 50px 0 0;\n  -webkit-transform: rotate(35deg);\n  -moz-transform: rotate(35deg);\n  transform: rotate(35deg);\n  background-color: yellow;",
+            after: "}\n\n@keyframes chomp1 {\n  0%   { transform: rotate(-35deg); }\n  50%   { transform: rotate(0deg); }\n  100% { transform: rotate(-35deg); }\n}\n\n@keyframes chomp2 {\n  0%   { transform: rotate(35deg); }\n  50%   { transform: rotate(0deg); }\n  100% { transform: rotate(35deg); }\n}",
+            rows: 1
         }
     ]
 };
